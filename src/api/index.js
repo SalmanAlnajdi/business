1b442be637d519c2d18296bc102dc7d4fcd9743a
SalmanAlnajdi <salman.it095@gmail.com>
2024-07-16 21:01:21 +0300

    business web setup

---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://localhost:8000",
+});
+
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `bearer ${token}`;
+  }
+  return config;
+});
+
+export default instance;
